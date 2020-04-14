3e8855acfe9be7fca08e596732b813713e519ada
Daryl Stark <daryl@dstark.nl>
2020-04-14 15:57:53 +0200

    Created a menu for the options button

--- src/google-app-engine/web-gui-v1/static-src/vue/src/components/me-dashboard-button-sidebar.vue
+++ src/google-app-engine/web-gui-v1/static-src/vue/src/components/me-dashboard-button-sidebar.vue
@@ -1,13 +1,35 @@
 <!-- Vue component for the a 'menu button' in the dashboard header -->
 <template>
-  <div id='me-dashboard-button-sidebar' v-if='$store.state.ui.sidebar_available'>
+  <div id='me-dashboard-button-sidebar'>
+
+    <div class="ui pointing dropdown top right" ref='dropdown'>
       <i class="ellipsis vertical icon"></i>
+      <div class="menu">
+        <div class="item" v-if='$store.state.ui.sidebar_available'>
+          <i class="bars icon"></i>
+            Show menu
+        </div>
+        <div class="item">
+          <i class="redo icon"></i>
+            Reload page
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <!-- The script that gets exported from the file -->
 <script>
+
+import '../../semantic/dist/semantic'
+import '../../semantic/dist/components/dropdown'
+
 export default {
-  name: 'me-dashboard-button-sidebar'
+  name: 'me-dashboard-button-sidebar',
+  mounted: function() {
+    // Add the 'dropdown' functionality of Semantic UI to the dropdown
+    $(this.$refs.dropdown).dropdown({ action: 'hide' });
+  }
 }
 </script>
