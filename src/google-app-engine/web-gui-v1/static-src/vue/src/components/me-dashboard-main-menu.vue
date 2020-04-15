6e573b74bf49985fc2dfcc65f2f015e020b95bbc
Daryl Stark <daryl@dstark.nl>
2020-04-15 16:16:38 +0200

    Added a second menu

--- src/google-app-engine/web-gui-v1/static-src/vue/src/components/me-dashboard-main-menu.vue
+++ src/google-app-engine/web-gui-v1/static-src/vue/src/components/me-dashboard-main-menu.vue
@@ -1,12 +1,32 @@
 <!-- Vue component for the login-form -->
 <template>
   <div id='me-dashboard-main-menu'>
-    <div v-for='menuitem in menuitems' class='menu-section'>
-      <h1>{{ menuitem.group }}</h1>
-      <ul>
-        <li v-for='subitem in menuitem.items'><i v-bind:class='[ subitem.icon, "icon" ]'></i><span>{{ subitem.title }}</span></li>
-      </ul>
+    <!--Component-->
+    <div class='menu' id='menu-main'>
+      <div v-for='menuitem in menuitems' class='menu-section'>
+        <h1>{{ menuitem.group }}</h1>
+        <ul>
+          <li v-for='subitem in menuitem.items'><i v-bind:class='[ subitem.icon, "icon" ]'></i><span>{{ subitem.title }}</span></li>
+        </ul>
+      </div>
     </div>
+    <!--/Component-->
+    <!--Component-->
+    <div class='menu menu-second' id='menu-notebook'>
+      <div class='menu-section'>
+        <h1>Notes</h1>
+        <ul>
+          <li><i class='folder open outline icon'></i><span>Een label</span></li>
+          <li><i class='folder open outline icon'></i><span>Een label</span></li>
+          <li><i class='folder open outline icon'></i><span>Een label</span></li>
+          <li><i class='folder open outline icon'></i><span>Een label</span></li>
+          <li><i class='markdown icon'></i><span>Een notitie</span></li>
+          <li><i class='markdown icon'></i><span>Een notitie</span></li>
+          <li><i class='markdown icon'></i><span>Een notitie</span></li>
+        </ul>
+      </div>
+    </div>
+    <!--/Component-->
   </div>
 </template>
 
