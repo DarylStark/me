94a5cf32195c8d66d43be4b615e912987653f441
Daryl Stark <daryl@dstark.nl>
2020-04-20 16:44:06 +0200

    Started with pages

---
+++ src/google-app-engine/web-gui-v1/static-src/vue/src/components/me-page-title.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='me-page-title'>
+    <div class='title'>
+      <i v-bind:class='[ { icon: icon }, icon ]' v-if='icon'></i>
+      <slot></slot>
+    </div>
+    <div class='actions'>
+      <slot name='actions'></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'me-page-title',
+  props: {
+      icon: { type: String, default: false }
+  }
+}
+</script>
--- src/google-app-engine/web-gui-v1/static-src/vue/src/pages/me-userprofile.vue
+++ src/google-app-engine/web-gui-v1/static-src/vue/src/pages/me-userprofile.vue
@@ -1,19 +1,36 @@
 <template>
   <div id='me-content-userprofile'>
-    <me-h1>
-      <i class="user circle icon"></i>
-      User Profile
-    </me-h1>
+    <me-page-title icon='user circle'>
+      User profile
+      <template v-slot:actions>
+        <!-- Component -->
+        <button class='ui primary button' disabled>Save</button>
+        <!-- /Component -->
+      </template>
+    </me-page-title>
+    <me-grid>
+      <me-grid-column>
+        <me-card raised>
+          Hier kun je je profiel editten
+        </me-card>
+      </me-grid-column>
+    </me-grid>
   </div>
 </template>
 
 <script>
-import me_h1 from '../components/me-h1'
+import me_page_title from '../components/me-page-title'
+import me_grid from '../components/me-grid'
+import me_grid_column from '../components/me-grid-column'
+import me_card from '../components/me-card'
 
 export default {
   name: 'me-content-userprofile',
   components: {
-    'me-h1': me_h1
+    'me-page-title': me_page_title,
+    'me-grid': me_grid,
+    'me-grid-column': me_grid_column,
+    'me-card': me_card
   },
   created: function() {
     // We don't need a sidebar on this page. Disable it.
