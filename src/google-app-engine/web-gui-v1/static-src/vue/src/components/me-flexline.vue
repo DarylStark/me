da718f4263ca10d70954c7898a9203c358602d8f
Daryl Stark <daryl@dstark.nl>
2020-04-28 19:32:44 +0200

    Created a flexline element

---
+++ src/google-app-engine/web-gui-v1/static-src/vue/src/components/me-flexline.vue
@@ -0,0 +1,12 @@
+<!-- Vue component for the login-form -->
+<template>
+  <div class='me-flexline'>
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'me-flexline'
+}
+</script>
