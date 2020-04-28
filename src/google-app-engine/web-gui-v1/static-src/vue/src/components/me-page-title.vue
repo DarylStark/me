0227d420ed732cba93f80eee546b45ebea1a13f3
Daryl Stark <daryl@dstark.nl>
2020-04-28 19:51:53 +0200

    Migrated the page title to a flexline

--- src/google-app-engine/web-gui-v1/static-src/vue/src/components/me-page-title.vue
+++ src/google-app-engine/web-gui-v1/static-src/vue/src/components/me-page-title.vue
@@ -1,18 +1,26 @@
 <template>
   <div class='me-page-title'>
-    <div class='title'>
-      <i v-bind:class='[ { icon: icon }, icon ]' v-if='icon'></i>
-      <slot></slot>
-    </div>
-    <div class='actions'>
-      <slot name='actions'></slot>
-    </div>
+    <me-flexline>
+      <div class='title'>
+        <i v-bind:class='[ { icon: icon }, icon ]' v-if='icon'></i>
+        <slot></slot>
+      </div>
+      <div class='spacer'></div>
+      <div class='actions'>
+        <slot name='actions'></slot>
+      </div>
+    </me-flexline>
   </div>
 </template>
 
 <script>
+import me_flexline from './me-flexline'
+
 export default {
   name: 'me-page-title',
+  components: {
+    'me-flexline': me_flexline
+  },
   props: {
       icon: { type: String, default: false }
   }
