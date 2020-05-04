839aea606025baf194436ce6210fcd13b1f5625e
Daryl Stark <daryl@dstark.nl>
2020-05-04 18:02:30 +0200

    Created the 'reveal key' button on the userprofile

--- src/google-app-engine/web-gui-v1/static-src/vue/src/components/me-userprofile-api-user.vue
+++ src/google-app-engine/web-gui-v1/static-src/vue/src/components/me-userprofile-api-user.vue
@@ -11,10 +11,19 @@
       <div class='actions'>
         <div class='ui icon red button' data-tooltip='Disable user token' data-position='top left' v-if='user_token.enabled' ><i class='power off icon'></i></div>
         <div class='ui icon green button' data-tooltip='Enable user token' data-position='top left' v-if='!user_token.enabled'><i class='play icon'></i></div>
-        <div class='ui icon button' data-tooltip='Reveal token' data-position='top left'><i class='key icon'></i></div>
+        <div class='ui icon button' data-tooltip='Reveal token' data-position='top left' v-on:click='show_token = !show_token'><i class='key icon'></i></div>
         <div class='ui icon button' data-tooltip='Rename token' data-position='top center'><i class='edit icon'></i></div>
         <div v-bind:class='[ "ui", { disabled: !user_token.expiration }, "icon", "button" ]' data-tooltip='Refresh token' data-position='top right'><i class='sync icon'></i></div>
         <div class='ui icon red button' data-tooltip='Remove token' data-position='top right'><i class='trash icon'></i></div>
+      </div>
+    </me-flexline>
+    <me-flexline  v-if='show_token' class='token_line'>
+      <div class='grower' ref='token'>{{ user_token.token }}</div>
+      <div class='ui icon button' data-tooltip='Copy token' data-position='top right' v-on:click='copy_token' v-if='!copied'>
+        <i class='copy icon'></i>
+      </div>
+      <div class='ui button' data-tooltip='Copy token' data-position='top right' v-on:click='copy_token' v-if='copied'>
+        Copied to clipboard!
       </div>
     </me-flexline>
   </div>
@@ -29,6 +38,27 @@ export default {
     expire: function() {
       let date_options = { year: 'numeric', month: 'long', day: 'numeric' };
       return this.user_token.expiration.toLocaleTimeString(undefined, date_options);
+    }
+  },
+  data: function() {
+    return {
+      show_token: false,
+      copied: false
+    }
+  },
+  methods: {
+    copy_token: function() {
+      // Copy the token code to the clipboard
+
+      // Local this
+      let vue_this = this;
+
+      // Copy the token the the clipboard
+      navigator.clipboard.writeText(this.user_token.token).then(function() {
+        vue_this.copied = true;
+
+        setTimeout(function() { vue_this.copied = false; }, 2000);
+      });
     }
   },
   components: {
